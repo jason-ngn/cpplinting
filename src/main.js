425c32a6a64bfa1f7ba3eae4aeb6d1a0b7b1b2a1
Jason <bennguyen20122006@gmail.com>
2025-10-31 21:14:29 -0700

    Modified linting

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import { wait } from './wait.js'
+import { getExecOutput } from '@actions/exec'
 
 /**
  * The main function for the action.
@@ -8,20 +8,10 @@ import { wait } from './wait.js'
  */
 export async function run() {
   try {
-    const ms = core.getInput('milliseconds')
-
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
+    const { stdout } = await getExecOutput('cpplint *.cc *.h')
+    core.info(stdout)
+  } catch (err) {
+    core.error(err)
+    core.setFailed('Your code is not linted properly.')
   }
 }
